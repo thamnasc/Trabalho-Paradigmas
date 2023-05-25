507c3f8b41753d6c6d64dc7a1341b8f85632c139
isabela <isabela.barata@ecomp.co>
2023-05-24 22:26:57 -0300

    [ADD] metodo toString adicionado a classe jogador

--- Jogador.java
+++ Jogador.java
@@ -1,7 +1,13 @@
 public class Jogador extends Entidade implements Movimentacao {
-    private Item item;
+    //private Item item;
     
+    public Jogador (String label) {
+        super(label);
+    }
     public void movimentar(){
         System.out.println("Movimentando...");
     } 
+    public String toString(){
+        return String.format(" " + this.label + " ");
+    }
 }
