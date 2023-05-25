d98ec3d12074e7a816af81e7dc3a0833072115ba
isabela <isabela.barata@ecomp.co>
2023-05-24 23:01:41 -0300

    [ADD] adicionado metodo isXX

--- Setor.java
+++ Setor.java
@@ -2,6 +2,10 @@ public class Setor extends Entidade {
     public Setor (String label){
         super(label);
     }
+    
+    public boolean isXX(){
+        return this.label.equals("XX");
+    }
 
     public String toString(){
         return String.format(" " + this.label + "  ");
